074dd4ebfbdc5156ed3ea43fcc178d617c6e9693
Justin Baum <justinba1010@gmail.com>
2018-10-18 01:39:14 -0400

    Busy Beaver 2 States

---
+++ universal_turing_machine.erl
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%% Author:	Justin Baum									%%%%
+%%%% Purpose:	Busy Beaver Turing Machine					%%%%
+%%%% Date: 		18 October 2018 							%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-module(universal_turing_machine).
+
+-behaviour(gen_server).
